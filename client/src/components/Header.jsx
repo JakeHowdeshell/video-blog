adac338c99da055e3fb77116b8317b5938d71d3f
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2024-06-06 16:19:25 -0500

    created page for client side header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import AppNavbar from './Nav/Nav';
+
+export default function Header() {
+    return (
+        <AppNavbar />
+    )
+}
